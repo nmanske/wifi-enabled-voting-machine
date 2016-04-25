d90fcd33ffbd3e9d8975618c0bc5b4d18149034c
Nathan Manske <nathan.manske@utexas.edu>
2016-04-25 17:28:37 -0500

    Added files for Keil

---
+++ lcd.d
@@ -0,0 +1,7 @@
+.\lcd.o: LCD.c
+.\lcd.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\lcd.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\lcd.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
+.\lcd.o: ST7735.h
+.\lcd.o: LCD.h
+.\lcd.o: ../inc/tm4c123gh6pm.h
---
+++ switch.d
@@ -0,0 +1,3 @@
+.\switch.o: Switch.c
+.\switch.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\switch.o: ../inc/tm4c123gh6pm.h
