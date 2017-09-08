96a3c3fc511c2629ad9d5af5e5e56ddb1a7cfe53
Nathan Manske <nathan.manske@utexas.edu>
2017-09-08 00:13:58 -0500

    Remove a bunch of useless files

--- switch.d
+++
@@ -1,4 +0,0 @@
-.\switch.o: Switch.c
-.\switch.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\switch.o: ../inc/tm4c123gh6pm.h
-.\switch.o: ST7735.h
--- uart.d
+++
@@ -1,6 +0,0 @@
-.\uart.o: UART.c
-.\uart.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\uart.o: ../inc/tm4c123gh6pm.h
-.\uart.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
-.\uart.o: FIFO.h
-.\uart.o: UART.h
